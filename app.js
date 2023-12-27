40d25bd520229799497ff61e64cb1796be1fe911
zobariyahnoor <zobariyahnoor@gmail.com>
2023-12-27 19:36:20 +0500

    hgdjjs

--- app.js
+++ app.js
@@ -1,2 +1,63 @@
-alert("SMIT D")
-console.log(5+5+2+5+5+5+45555555555+554444444)
+// alert("SMIT D")
+// console.log(5+5+2+5+5+5+45555555555+554444444)
+// var firstname = "Pakistan"
+// console.log("Majid", 5+2,4+4,true, false);
+// var firstname = "Majid"
+// var lastname = "Iqbal"
+// console.log (firstname,lastname)
+// var firstname = "Aziz"
+// var lastname = "Jan"
+// console.log (firstname,lastname)
+// let numone = prompt("enter your first number")
+// let numetwo = prompt ("please enter your second number")
+// console.log(numone-numetwo)
+// let country = "Pakistan"
+// let continent = "Asia" 
+// let population = 23000000
+// console.log(country,continent,population)
+// let country = "Pakistan"
+// let continent = "Asia" 
+// let population = 23000000
+// console.log(country)
+// console.log(continent)
+// console.log(population)
+// let pakistan = true 
+// let Urdu = true
+// let 23000000 = true
+// console.log(pakistan)
+// console.log(urdu)
+// console.log(population)
+// let num1 = 9;
+// let num2 = 5;
+// let res = num1 % num2;
+// console.log(res);
+// let num3 = 9;
+// let num4 = 5;
+// let res2 = num3 + num4;
+// console.log(res2);
+// let num5 = prompt("please enter the 1st num");
+// let num6 = prompt("please enter the 2nd num");
+// let res3 = num5 * num6;
+// alert(res3);
+
+// let pak = 50;
+// let rest = pak / 2;
+// console.log(rest);
+
+// let pak2 = 50;
+// console.log(pak2 + 1);
+// let pak3 = 80;
+// console.log(pak3 - 2);
+// let pak = 50;
+// console.log(pak++);
+// console.log(pak);
+// let pak2 = 75;
+// console.log(pak2--);
+// console.log(pak2)
+let asia = 2000;
+console.log(asia++);
+console.log(asia);
+
+
+
+
