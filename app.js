1f5a772edbb8605b233b34f0c34a9b262bda211d
unknown <AJ@DESKTOP-A0FD8AE.AJ_TRUST>
2024-01-24 19:39:51 +0500

    bst

--- app.js
+++ app.js
@@ -54,9 +54,26 @@
 // let pak2 = 75;
 // console.log(pak2--);
 // console.log(pak2)
-let asia = 2000;
-console.log(asia++);
-console.log(asia);
+// let asia = 2000;
+// console.log(asia++);
+// console.log(asia);
+
+// let cities=["karachi",'lahore','islamabad']
+// console.log('welcome to '+cities[2])
+
+// let Grade=["1",'2','3','4','5']
+// console.log(Grade);
+// Grade.push("5");
+// console.log(Grade)
+// Grade.pop()
+// console.log(Grade)
+// console.log(Grade.pop())
+let grade=["a", "b", "c", "d", "e", "f"]
+console.log(grade);
+grade.unshift("b","1")
+console.log(grade)
+grade.shift()
+console.log(grade)
 
 
 
